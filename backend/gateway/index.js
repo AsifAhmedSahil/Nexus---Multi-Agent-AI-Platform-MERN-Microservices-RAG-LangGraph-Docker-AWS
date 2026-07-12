b5625d240941e1b96af574a06bd3f77f89beba5a
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2026-07-12 10:48:30 +0600

    authentication created

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,14 +1,34 @@
 import express from "express"
 import dotenv from "dotenv"
 import proxy from "express-http-proxy"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
 const port = process.env.PORT 
 
 const app = express()
+app.use(express.json());
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true 
+}))
 
-app.use("/auth",proxy(process.env.AUTH_SERVICE))
+app.use(cookieParser())
+
+
+
+// app.use("/auth",proxy(process.env.AUTH_SERVICE))
+
+app.use(
+  "/auth",
+  proxy(process.env.AUTH_SERVICE, {
+    proxyResOptDecorator: (proxyResOpts) => {
+      return proxyResOpts;
+    }
+  })
+);
 
 app.listen(port,()=>{
     console.log(`gateway started at port: ${port}`)
