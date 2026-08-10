2616ab3bdecffebd723bd330728a4887dc74fa41
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2026-08-10 22:35:09 +0600

    image to text agent

--- backend/services/agent/routes/agent.route.js
+++ backend/services/agent/routes/agent.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { agent } from "../controllers/agent.controller.js"
+import multer from "../config/multer.js"
 
 export const router = express.Router()
 
 
-router.post("/chat",agent)
+router.post("/chat",multer.single("file"),agent)
 
 
 
